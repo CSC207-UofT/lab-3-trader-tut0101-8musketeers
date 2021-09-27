7489b267b5f42395394be603c258991d2fe12abc
kevin <kevin.j.wang@gmail.com>
2021-09-27 11:36:09 -0400

    fixed errors

--- src/main/java/Tradable.java
+++ src/main/java/Tradable.java
@@ -1,9 +1,11 @@
 public interface Tradable {
-    private int secretNumber = 20;
+    int secretNumber = 20;
     int getPrice();
     /*
     retruns teh coloer of the thing that ur trading if it has one i guess
      */
     String getColor();
+
+    String dangerLevel();
 
 }
